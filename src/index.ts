f1895e204af5414a43cd5a74566354e76da68541
Khushal Agarwal <starkhush5@gmail.com>
2024-02-09 15:56:11 +0530

    fix : change runtime bun -> node

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { showRoutes } from "hono/dev";
 import { logger } from "hono/logger";
+import { serve } from "@hono/node-server";
 import { prettyJSON } from "hono/pretty-json";
 import { HTTPException } from "hono/http-exception";
+import { serveStatic } from "@hono/node-server/serve-static";
 
 import "./lib/env"; // This is validation for the environment variables early
 
@@ -16,7 +18,7 @@ const app = new Hono({ strict: false }); // match routes w/ or w/o trailing slas
  * Middlewares
  * -----------------------------------------------------------------------------------------------*/
 
-app.use("*", cors(), prettyJSON(), logger());
+app.use("*", cors(), prettyJSON(), logger(), serveStatic({ root: "public" }));
 
 /* -----------------------------------------------------------------------------------------------
  * Routes
@@ -31,47 +33,49 @@ app.route("/ping", ping);
 
 /* 404 */
 app.notFound((c) => {
-	return c.json(
-		{
-			status: "FAILED",
-			message: "🚫 Requested route not found, please enter a valid route",
-			error: {
-				code: "invalid_route",
-				routes,
-			},
-		} satisfies ServerResponse,
-		404,
-	);
+  return c.json(
+    {
+      status: "FAILED",
+      message: "🚫 Requested route not found, please enter a valid route",
+      error: {
+        code: "invalid_route",
+        routes,
+      },
+    } satisfies ServerResponse,
+    404
+  );
 });
 
 /* -----------------------------------------------------------------------------------------------
  * Global Error Handler
  * -----------------------------------------------------------------------------------------------*/
 app.onError((err, c) => {
-	if (err instanceof HTTPException) {
-		return c.json(
-			{
-				status: "ERROR",
-				error: "unauthorized",
-				message:
-					"❌ You are not authorized to access this resource, please provide a valid token",
-			} satisfies ServerResponse,
-			err.status,
-		);
-	}
+  if (err instanceof HTTPException) {
+    return c.json(
+      {
+        status: "ERROR",
+        error: "unauthorized",
+        message:
+          "❌ You are not authorized to access this resource, please provide a valid token",
+      } satisfies ServerResponse,
+      err.status
+    );
+  }
 
-	console.error(err);
+  console.error(err);
 
-	return c.json(
-		{
-			status: "ERROR",
-			message: `❌ ${err.message}`,
-			error: err.name,
-		} satisfies ServerResponse,
-		400,
-	);
+  return c.json(
+    {
+      status: "ERROR",
+      message: `❌ ${err.message}`,
+      error: err.name,
+    } satisfies ServerResponse,
+    400
+  );
 });
 
 showRoutes(app, { colorize: true });
+
+serve(app);
 
 export default app;
